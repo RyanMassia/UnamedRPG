91ac3a6940dff6d6f4ffbe2bf41408619da67bf7
Ryan Massia <ryanmassia6@gmail.com>
2021-01-24 11:48:55 -0500

    added itemobject scripts

---
+++ UnamedRPG/Assets/ScriptableObjectts/Items/Scripts/EquipmentObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Equipment Object", menuName = "Inventroy System/Items/Equipment")]
+
+public class EquipmentObject : ItemObject
+{
+    public float attackBonus;
+    public float defenseBonus;
+
+    public void Awake()
+    {
+        type = ItemType.Equpment;
+    }
+}
---
+++ UnamedRPG/Assets/ScriptableObjectts/Items/Scripts/RecoveryObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Recovery Object", menuName = "Inventroy System/Items/Recovery")]
+
+public class RecoveryObject : ItemObject
+{
+    public int restoreHpValue;
+    public void Awake()
+    {
+        type = ItemType.Recovery;   
+    }
+
+    
+}
